db04f33061e8b237c2b366cee5ca21732022f473
Maciej Obuchowski <maciej.obuchowski@getindata.com>
2021-05-11 17:22:14 +0200

    first ci
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@getindata.com>

--- integrations/dbt/setup.py
+++ integrations/dbt/setup.py
@@ -3,17 +3,26 @@ from setuptools import find_packages
 from setuptools import setup
 
 package_name = "dbt-openlineage"
-package_version = "0.14.1"
+package_version = "0.14.2"
 description = """The openlineage bigquery adapter plugin for dbt (data build tool)"""
+
+extras_require = {
+    "tests": [
+        "pytest",
+        "pytest-cov",
+        "mock",
+        "flake8",
+    ],
+}
+extras_require["dev"] = set(sum(extras_require.values(), []))
+
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
-    author='OpenLineage Plugin Authors',
-    author_email='openlineage@openlineage.com',
-    url='openlinage.github.io',
+    author='Marquez Project',
     packages=find_packages(),
     package_data={
         'dbt': [
@@ -25,5 +34,7 @@ setup(
         "dbt-core==0.20.0b2",
         "sqlparse==0.4.1",
         "openlineage==0.0.1"
-    ]
+    ],
+    extras_require=extras_require,
+    python_requires=">=3.6",
 )
